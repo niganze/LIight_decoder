53c065f343d1012843bef48d6b804ac57860d119
igizeneza <igizeneza100@gmail.com>
2023-08-31 16:50:26 +0200

    master

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,23 +1,25 @@
 // import React from 'react'
-
+import FF from '../assets/battery.jpg'
+import YY from '../assets/hy.jpg'
+import GG from '../assets/photo1.jpg'
 function Cards() {
   return (
     <div>
       <div className="card_container">
         <div className="single_card">
-            <img src="" alt="hbhjbjklnljk" />
+            <img src={YY} alt="hbhjbjklnljk" />
             <h4>Lorem ipsum dolo.</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Expedita inventore eius,placeat, </p>
         </div>
         <div className="single_card">
-            <img src="" alt="hbhjbjklnljk" />
+            <img src={FF} alt="hbhjbjklnljk" />
             <h4>Lorem ipsum dolo.</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Expedita inventore eius,placeat, </p>
         </div>
         <div className="single_card">
-            <img src="" alt="hbhjbjklnljk" />
+            <img src={GG} alt="hbhjbjklnljk" />
             <h4>Lorem ipsum dolo.</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Expedita inventore eius,placeat, </p>
